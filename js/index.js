f10293f24887302ed3ac1df47eb9377415a5d73f
Gerald Walker <gerald.e.walker05@gmail.com>
2023-09-22 16:25:43 -0500

    end of work friday

--- js/index.js
+++ js/index.js
@@ -5,12 +5,42 @@ import {getMovies, getMovie, deleteMovie, postMovie, searchMovieByTitle, patchMo
 
 
 (async()=>{
+
     const movies = await getMovies();
     console.log(movies);
     for (let movie of movies) {
         const target = document.querySelector(".movies-grid")
         renderMovie(movie,target);
+
+
     }
 
+    // $(".deletebtn").on("click",async function () {
+    //     $(this)
+    //     deleteMovie(this.id)
+    //     let movies = await getMovies()
+    //     renderMovie(movies)
+    // })
+
+    // const thing = movie.id
+    // const deleteBtn = document.querySelector(`.movies-card[data-id="${thing}"]`)
+    // console.log(deleteBtn);
+    // deleteBtn.addEventListener("click", () => {
+    //     // alert(`${movie.title} was deleted.`)
+    //     alert('boom')
+    // })
+
+
+    // const editbtn = document.getElementById("edit-btn");
+    //
+    // editbtn.onclick = function() {
+    //     modal.style.display = "block"; }
+
+    // const deletebtn = document.querySelector(".delete-btn");
+    //
+    // deletebtn.onclick = function () {
+    //     deleteMovie(5);
+    //     console.log("clicked")
+    // }
 
 })();
