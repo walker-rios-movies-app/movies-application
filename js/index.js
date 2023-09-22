da3c3a3e82ab63f544fe233dc54ce1169b4d0063
Milianigia Rios <nanirios671@gmail.com>
2023-09-22 14:37:48 -0500

    added loading bar and error message

--- js/index.js
+++ js/index.js
@@ -2,7 +2,19 @@
 
 import {getMovies, getMovie, deleteMovie, postMovie, searchMovieByTitle, patchMovie, renderMovie} from "./api/movies.js"
 
-
+document.onreadystatechange = function () {
+    if (document.readyState !== "complete") {
+        document.querySelector(
+            "body").style.visibility = "hidden";
+        document.querySelector(
+            "#loader").style.visibility = "visible";
+    } else {
+        document.querySelector(
+            "#loader").style.display = "none";
+        document.querySelector(
+            "body").style.visibility = "visible";
+    }
+};
 
 (async()=>{
     const movies = await getMovies();
@@ -11,6 +23,5 @@ import {getMovies, getMovie, deleteMovie, postMovie, searchMovieByTitle, patchMo
         const target = document.querySelector(".movies-grid")
         renderMovie(movie,target);
     }
-
 
 })();
