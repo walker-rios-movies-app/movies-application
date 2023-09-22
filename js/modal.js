95efda90e90d490ef3a77d1f984772ad409ee063
Milianigia Rios <nanirios671@gmail.com>
2023-09-22 09:12:22 -0500

    figured out the object

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,13 @@
 
-var modal = document.getElementById("myModal");
+const modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("add-movie-btn");
+const btn = document.getElementById("add-movie-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+const span = document.getElementsByClassName("close")[0];
+
+const submit = document.getElementById("submit")
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -22,6 +24,24 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+submit.onclick = function(event) {
+    const title = document.getElementById('title').value;
+    const year = document.getElementById('year').value;
+    const summary = document.getElementById('summary').value;
+    const rating = document.getElementById('rating').value;
+    const genre = document.getElementById('genre').value;
+
+    const movieObject = {
+        title: title,
+        year: year,
+        summary:summary,
+        rating:rating,
+        genre:genre
+    }
+    console.log(movieObject)
+
 }
 
 let nameInput = document.getElementById('myModal');
